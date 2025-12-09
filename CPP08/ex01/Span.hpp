3a295915f5b7b02835b5e41f08aab02851d913f8
onevilx <youssefaboukir7@gmail.com>
2025-12-09 12:49:01 +0100

    pushingtocluster

--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -3,6 +3,17 @@
 
 #include <iostream>
 
+class Span
+{
+    public:
+        Span();
+        Span(unsigned int N);
+        Span(const Span& other);
+        Span& operator=(const Span& content);
+        ~Span();
 
+        void addNumber();
+        
+};
 
 #endif

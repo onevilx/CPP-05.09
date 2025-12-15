fbe65800eb919c132f54099463e449e5b9374602
Youssef Aboukir <yaboukir@e2r3p9.1337.ma>
2025-12-15 20:56:33 +0100

    finished

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -16,28 +16,21 @@ class MutantStack : public std::stack<T, Container>
 
         typedef typename container_type::iterator           iterator;
         typedef typename container_type::const_iterator     const_iterator;
-        typedef typename container_type::reverse_iterator   reverse_iterator;
-        typedef typename container_type::const_reverse_iterator const_reverse_iterator;
 
     MutantStack() : base_type() {}
     MutantStack(const MutantStack& other) : base_type(other) {}
-    MutantStack& operator=(const MutantStack& other)
+    MutantStack& operator=(const MutantStack& content)
     {
-        if (this != &other)
-            base_type::operator=(other);
+        if (this != &content)
+            base_type::operator=(content);
         return *this;
     }
     ~MutantStack() {}
 
     iterator begin()              { return this->c.begin(); }
     iterator end()                { return this->c.end(); }
-    reverse_iterator rbegin()     { return this->c.rbegin(); }
-    reverse_iterator rend()       { return this->c.rend(); }
-
     const_iterator begin() const              { return this->c.begin(); }
     const_iterator end()   const              { return this->c.end(); }
-    const_reverse_iterator rbegin() const     { return this->c.rbegin(); }
-    const_reverse_iterator rend()   const     { return this->c.rend(); }
 };
 
 #endif

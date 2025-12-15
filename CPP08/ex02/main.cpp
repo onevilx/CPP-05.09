fb58f59776dd0328d3559d93c1a33bd0b161c85e
Youssef Aboukir <yaboukir@e2r3p9.1337.ma>
2025-12-15 18:47:19 +0100

    finished

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -23,23 +23,5 @@ int main()
         ++it;
     }
     std::stack<int> s(mstack);
-
-    std::cout << "\n---- container list test ----" << std::endl;
-    std::list<int> lst;
-    lst.push_back(5);
-    lst.push_back(17);
-    lst.pop_back();
-    lst.push_back(3);
-    lst.push_back(5);
-    lst.push_back(737);
-    lst.push_back(0);
-
-    std::list<int>::iterator lit = lst.begin();
-    std::list<int>::iterator lite = lst.end();
-    while (lit != lite)
-    {
-        std::cout << *lit << std::endl;
-        ++lit;
-    }
     return 0;
 }
